b7044a8b79c204781a318c47d1741be23644658e
Yuliya Raikova <yuradev2001@gmail.com>
2022-12-29 11:40:15 +0300

    ...

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,11 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        try {
+            return Double.parseDouble(str) > 0;
+        }
+        catch (NumberFormatException | NullPointerException e) {
+            return false;
+        }
     }
 }
